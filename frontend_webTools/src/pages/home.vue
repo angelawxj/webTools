88b7ffcdab7b8034f914383b1dd2df80e90a96c3
angelawxj <1432352091@qq.com>
2019-06-06 14:41:11 +0800

    提交

--- frontend_webTools/src/pages/home.vue
+++ frontend_webTools/src/pages/home.vue
@@ -1,35 +1,45 @@
 <template>
   <div class="gdgg_xjbb_page">
-    <div class="gdgg_xjbb_rainbowBG"></div>
+    <div class="gdgg_xjbb_rainbowBG primary-bg"></div>
     <div class="gdgg_xjbb_page-margin">
       <el-row type="flex" :gutter="80">
         <el-col :span="4">
           <a @click="$router.push('financeReport')">
-            <div class="gdgg_xjbb_modular"><img class="gdgg_xjbb_icon" src="../assets/images/report.png" alt></div>
+            <div class="gdgg_xjbb_modular">
+              <img class="gdgg_xjbb_icon" src="../assets/images/report.png" alt>
+            </div>
             <span class="gdgg_xjbb_label">财务报表</span>
           </a>
         </el-col>
         <el-col :span="4">
           <a>
-            <div  class="gdgg_xjbb_modular"><img class="gdgg_xjbb_icon" src="../assets/images/business-and-finance.png" alt></div>
+            <div class="gdgg_xjbb_modular">
+              <img class="gdgg_xjbb_icon" src="../assets/images/business-and-finance.png" alt>
+            </div>
             <span class="gdgg_xjbb_label">现金流机器</span>
           </a>
         </el-col>
         <el-col :span="4">
           <a>
-            <div  class="gdgg_xjbb_modular"><img class="gdgg_xjbb_icon" src="../assets/images/Interview.png" alt></div>
+            <div class="gdgg_xjbb_modular">
+              <img class="gdgg_xjbb_icon" src="../assets/images/Interview.png" alt>
+            </div>
             <span class="gdgg_xjbb_label">面试刷题</span>
           </a>
         </el-col>
         <el-col :span="4">
           <a>
-            <div  class="gdgg_xjbb_modular"><img class="gdgg_xjbb_icon" src="../assets/images/skill.png" alt></div>
+            <div class="gdgg_xjbb_modular">
+              <img class="gdgg_xjbb_icon" src="../assets/images/skill.png" alt>
+            </div>
             <span class="gdgg_xjbb_label">技能树</span>
           </a>
         </el-col>
         <el-col :span="4">
           <a>
-            <div  class="gdgg_xjbb_modular"><img class="gdgg_xjbb_icon" src="../assets/images/library.png" alt></div>
+            <div class="gdgg_xjbb_modular">
+              <img class="gdgg_xjbb_icon" src="../assets/images/library.png" alt>
+            </div>
             <span class="gdgg_xjbb_label">图书馆</span>
           </a>
         </el-col>
@@ -57,7 +67,6 @@ export default {
 }
 .gdgg_xjbb_rainbowBG {
   position: absolute;
-  background: #e8a36c;
   height: 100%;
   overflow: hidden;
   width: 100%;
@@ -76,24 +85,24 @@ export default {
   background: #57bc56;
   z-index: 0;
 }
-.gdgg_xjbb_page-margin{
+.gdgg_xjbb_page-margin {
   width: 70%;
   min-width: 800px;
   margin: auto;
   padding: 40px 30px;
 }
-.gdgg_xjbb_modular{
+.gdgg_xjbb_modular {
   position: relative;
   height: 90px;
   border-radius: 4px;
 }
-.gdgg_xjbb_icon{
+.gdgg_xjbb_icon {
   display: block;
   width: 90px;
   position: absolute;
   top: 50%;
   left: 50%;
-  transform:translate(-50%,-50%);
+  transform: translate(-50%, -50%);
 }
 .gdgg_xjbb_label {
   display: block;
