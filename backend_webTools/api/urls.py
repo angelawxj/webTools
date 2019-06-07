a3e76879eba2ed209af9b8b3b13d6e9bcc79dabd
wangxiaojuan <1432352091@qq.comm>
2019-06-07 21:25:45 +0800

    修改前后端代码

--- backend_webTools/api/urls.py
+++ backend_webTools/api/urls.py
@@ -4,9 +4,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('Report/', views.ListReport.as_view()),
-    path('Report/<int:pk>/', views.DetailReport.as_view()),
-    path('Interview/', views.ListInterview.as_view()),
-    path('Interview/<int:pk>/', views.DetailInterview.as_view()),
+    path('report/', views.ListReport.as_view()),
+    path('report/<int:pk>/', views.DetailReport.as_view()),
+    path('interview/', views.ListInterview.as_view()),
+    path('interview/<int:pk>/', views.DetailInterview.as_view()),
 ]
 
